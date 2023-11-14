eafdfbbae4996ef560c4d25057c3335c3599f7b4
Alexis Demont <alexis.demont@protonmail.com>
2023-11-14 12:34:22 +0100

    IDF optimisé

--- IDF.py
+++ IDF.py
@@ -1,35 +1,41 @@
 # Écrire une fonction dont le  paramètre est le répertoire où se trouve l’ensemble des fichiers du corpus
 # Qui retourne un dictionnaire associant à chaque mot son score IDF.
 # Calculer le logarithme de l'inverse de la proportion de documents qui contiennent ce mot .
+# Le temps d'execution de la fonction IDF se situe entre 0.0015 et 0.003 secondes. 
+
 from math import log10
+import time
 from basicsFunctions import list_of_files
 
-def CheckWordExist(word, file, directory='./cleaned/'):
-    with open(directory+file, 'r') as file:
-        for line in file:
-            for ele in line.split():
-                if word==ele:
-                    return True
-        return False
-    
-def RegroupAllWords(directory='./cleaned/'):
-    list_files=list_of_files(directory,'.txt')
-    allWords=set()
+
+def create_file_words_dict(directory="./cleaned/"):
+    list_files = list_of_files(directory, ".txt")
+    file_words = {}
     for file in list_files:
-        with open(directory+file,'r') as file:
-            for line in file:
-                for word in line.split():
-                    allWords.add(word)
+        with open(directory + file, "r") as f:
+            words_in_file = set(word for line in f for word in line.split())
+        file_words[file] = words_in_file
+    return file_words
+
+def CheckWordExist(word, file, file_words):
+    return word in file_words[file]
+
+def RegroupAllWords(file_words):
+    allWords = set()
+    for words_in_file in file_words.values():
+        allWords.update(words_in_file)
     return allWords
 
-def CalculateIDF(directory='./cleaned/'):
-    list_files=list_of_files(directory,'.txt')
-    allWords=RegroupAllWords(directory)
-    nb_IDF_words={}
+
+def CalculateIDF(directory="./cleaned/"):
+    file_words=create_file_words_dict(directory)
+    list_files = list_of_files(directory, ".txt")
+    allWords = RegroupAllWords(file_words)
+    nb_IDF_words = {}
     for ele in allWords:
-        nb_word_existence=0
+        nb_word_existence = 0
         for file in list_files:
-            if CheckWordExist(ele,file):
-                nb_word_existence+=1
-        nb_IDF_words[ele]=log10(((len(list_files))/(nb_word_existence)))
+            if CheckWordExist(ele, file,file_words):
+                nb_word_existence += 1
+        nb_IDF_words[ele] = log10(((len(list_files)) / (nb_word_existence)))
     return nb_IDF_words
