aa2f4b01a965defcbdd8dcd39b0fd358046f9213
Alexis Demont <alexis.demont@protonmail.com>
2023-11-22 23:12:03 +0100

    fixed some mistakes. Now working as expected

--- motsnonimportants.py
+++ motsnonimportants.py
@@ -1,13 +1,15 @@
 # A partir de la matrice on crée un set des mots dont le score TfIdf= 0
 from TfIdf import TfIdf_Matrice
+
 def NotImportantWord():
-    matrice = TfIdf_Matrice(directory='./cleaned/')
-    matrice[0].insert(0, " ")
-    for word in matrice :
-        NotImportantsWords = set()
-        for ScoreWord in word :
-            if ScoreWord == 0 :
-                zero = 0
-        if zero == 0 :
-            NotImportantsWords.update(word)
+    matrice = TfIdf_Matrice(directory="./cleaned/")
+    NotImportantsWords = set()
+    for i in range(1, len(matrice)):
+        currentWord = matrice[i][0]
+        isNull = True
+        for j in range(1, len(matrice[i])):
+            if matrice[i][j] != 0:
+                isNull = False
+        if isNull:
+            NotImportantsWords.add(str(currentWord))
     return NotImportantsWords
