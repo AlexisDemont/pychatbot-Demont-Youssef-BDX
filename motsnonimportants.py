b4a9cafab6aac16d7261fadf8d2c7e5fe87c64a7
Alexis Demont <alexis.demont@protonmail.com>
2023-11-23 17:12:36 +0100

    Fonction motsnonimportants.py finalisée

--- motsnonimportants.py
+++ motsnonimportants.py
@@ -1,8 +1,13 @@
-# A partir de la matrice on crée un set des mots dont le score TfIdf= 0
 from TfIdf import TfIdf_Matrice
 
-def NotImportantWord():
-    matrice = TfIdf_Matrice(directory="./cleaned/")
+def NotImportantWord(directory="./cleaned/"):
+    """
+    Fonction qui retourne un set des mots dont le score TfIdf = 0 pour l'ensemble des textes
+    :param: répertoire des fichiers textes nettoyés
+    :return: set des mots dont le score TfIdf = 0 pour l'ensemble des textes
+    :rtype: set
+    """
+    matrice = TfIdf_Matrice(directory)
     NotImportantsWords = set()
     for i in range(1, len(matrice)):
         currentWord = matrice[i][0]
