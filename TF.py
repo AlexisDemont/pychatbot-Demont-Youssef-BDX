bc29699604319915c447d32d88fab1141de71007
Alexis Demont <alexis.demont@protonmail.com>
2023-11-26 20:20:13 +0100

    Fin allSaidWord.py ajouts de module pour le traitement des textes

--- TF.py
+++ TF.py
@@ -1,14 +1,18 @@
 # fonction dont le parametre est chaine de chr qui retourne un dictionnaire {nombreOccurence : mot}
 
-"""
+def extractWordsFromThis(string):
+    words = {word for word in string.split()}
+    return words
+
+def CalculateOccurenceWords(string):
+    """
     Fonction qui retourne un dictionnaire dont la clé est le mot et sa valeur est le nombre d'occurence pour chaque fichier
     :param: une chaine de caractere ( elle represente tout le contenu du fichier apres nettoyage )
     :return: dictionnaire {mot : nbOccurence}
     :rtype: dictionnaire
     """
-def CalculateOccurenceWords(string):
     nb_occurence = {}
-    words = string.split()
+    words = extractWordsFromThis(string)
     for word in words:
         if word not in nb_occurence:
             nb_occurence[word] = 1
