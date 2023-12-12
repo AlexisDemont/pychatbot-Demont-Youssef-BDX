24fae9da5353add01568616b5f16e5e2737a8899
sarahyoussef33 <sarah.youssef@efrei.net>
2023-12-12 12:05:47 +0100

    Commenter et modifier la fonction + PROBLEME

--- wordDocQuest.py
+++ wordDocQuest.py
@@ -1,12 +1,23 @@
 " importer la liste des mots fct1 "
 
 from utils import tfidf_matrice
-def SearchCommonWords (string , file) :
-    WordsInQuestion = fct1
+def SearchCommonWords (string , directory) :
+    """
+        Function that returns a list of commons words between the question and the documents
+
+        Parameters:
+            A string
+
+        Returns:
+            list of common words
+
+        """
+    WordsInQuestion = ['le','climat'] # fonction pour la liste des mots
     CommonWords = []
     for element in WordsInQuestion :
-        for word in tfidf_matrice () :
-            if element = word :
+        for word in tfidf_matrice (directory) :
+            if element == word :
                 CommonWords.append(element)
 
     return CommonWords
+test = print(SearchCommonWords("le climat ", './cleaned/'))
