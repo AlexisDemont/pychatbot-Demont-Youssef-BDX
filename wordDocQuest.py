0b0b6e2004164ae41f2bdce1cf320e3d072817fd
sarahyoussef33 <sarah.youssef@efrei.net>
2023-12-12 11:49:06 +0100

    Ajout de la branche fonction2 : intersection des mots entre question et document

---
+++ wordDocQuest.py
@@ -0,0 +1,13 @@
+" importer matrice tfidf "
+" importer la liste des mots "
+
+def SearchCommonWords (string , file) :
+
+    WordsInQuestion = fct
+    CommonWords = []
+    for element in WordsInQuestion :
+        for word in matricetfidf :
+            if element = word :
+                CommonWords.append(element)
+
+    return CommonWords
