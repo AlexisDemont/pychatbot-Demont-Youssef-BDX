dc702b6dd3a734a50c97df0070830d8a0b2d8930
sarahyoussef33 <sarah.youssef@efrei.net>
2023-12-12 11:54:54 +0100

    importer focntion tfidf vers la focntion

--- wordDocQuest.py
+++ wordDocQuest.py
@@ -1,12 +1,11 @@
-" importer matrice tfidf "
-" importer la liste des mots "
+" importer la liste des mots fct1 "
 
+from utils import tfidf_matrice
 def SearchCommonWords (string , file) :
-
-    WordsInQuestion = fct
+    WordsInQuestion = fct1
     CommonWords = []
     for element in WordsInQuestion :
-        for word in matricetfidf :
+        for word in tfidf_matrice () :
             if element = word :
                 CommonWords.append(element)
 
