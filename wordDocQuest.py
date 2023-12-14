7e3a9408694a8858eac8a4e2899ff398981c981f
sarahyoussef33 <sarah.youssef@efrei.net>
2023-12-14 15:09:08 +0100

    Modifier

--- wordDocQuest.py
+++ wordDocQuest.py
@@ -1,20 +1,22 @@
 " importer la liste des mots fct1 "
 from utils import create_file_words_dict
-from utils import tfidf_matrice
-file_words = create_file_words_dict("./cleaned")
-all_words = set().union(*file_words.values())
+from text_organization import string_cleaner
+
 def SearchCommonWords (string , directory) :
     """
         Function that returns a list of commons words between the question and the documents
 
         Parameters:
-            A string
+            A string and directory
 
         Returns:
             list of common words
 
         """
-    WordsInQuestion = ['le','climat'] # fonction pour la liste des mots
+    WordString = string_cleaner(string)
+
+    file_words = create_file_words_dict(directory)
+    all_words = set().union(*file_words.values())
 
     CommonWords = []
     for element in WordsInQuestion :
@@ -22,5 +24,3 @@ def SearchCommonWords (string , directory) :
             CommonWords.append(element)
 
     return CommonWords
-test = print(SearchCommonWords("le climat ", './cleaned/'))
-# le probleme vient apparemment de la matrice tf-idf car si on teste tf idf dans utils il y a un souci
